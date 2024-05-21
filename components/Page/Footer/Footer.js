57b7dbc9a1532c356fa827dcd28c84ceaabc2c1c
sahilsian <sahilsian101@gmail.com>
2024-05-21 13:51:24 -0300

    404 page

--- components/Page/Footer/Footer.js
+++ components/Page/Footer/Footer.js
@@ -4,6 +4,7 @@ import Image from "next/image"
 import Link from "next/link"
 import siteConfig from "../../../site.config"
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons"
+import { Paragraph } from "../../Core/Paragraph"
 
 export const Footer = ({ items }) => {
     return (
@@ -13,19 +14,22 @@ export const Footer = ({ items }) => {
                 <div className="flex-1 w-full">
 
                     <div className="flex-1 min-w-[300px] max-w-[500px]">
-                        <Link href={"/"}>
-                        <Image alt="Light Logo" className="my-7" width={200} height={63} objectFit="cover" src={'/logo.png'}></Image>
-                        </Link>
-                        <h4 className="text-[18px] pb-8">{siteConfig.texts.footer.footertext}</h4>
+                        <div className="max-w-[150px]">
+                            <Link href={"/"}>
+                                <Image alt="Light Logo" className="my-7" width={200} height={63} objectFit="cover" src={'/logo.png'}></Image>
+                            </Link>
+                        </div>
+
+                        <Paragraph content={siteConfig.texts.footer.footertext}></Paragraph>
 
                         <div className="flex items-center gap-5 mb-3">
                             <FontAwesomeIcon icon={faEnvelope} width={26}></FontAwesomeIcon>
-                            <h4 className="text-[24px] font-semibold">{siteConfig.texts.footer.email}</h4>
+                            <h4 className="text-[22px] font-medium">{siteConfig.texts.footer.email}</h4>
                         </div>
 
                         <div className="flex items-center gap-5 mb-7">
                             <FontAwesomeIcon icon={faPhone} width={26}></FontAwesomeIcon>
-                            <h4 className="text-[24px] font-semibold">{siteConfig.texts.footer.phone}</h4>
+                            <h4 className="text-[22px] font-medium">{siteConfig.texts.footer.phone}</h4>
                         </div>
                         <div className="flex items-center gap-5 mb-3">
                             <Link className="hover:opacity-75 transition-all" href={siteConfig.texts.footer.instagram}>
@@ -40,7 +44,7 @@ export const Footer = ({ items }) => {
 
                 <div className="flex-1 text-[18px] py-4 min-w-[300px]">
                     <div>
-                        <h2 className="text-[1.7rem] lg:text-[2rem] pt-6 mb-3 font-bold">Pages</h2>
+                        <h2 className="text-[1.7rem] lg:text-[2rem] mb-3 font-medium">Pages</h2>
                     </div>
                     <div className="flex flex-col flex-wrap max-h-[280px]">
                     {items.map((item)=> {
@@ -62,7 +66,7 @@ export const Footer = ({ items }) => {
                 </div>
             </div>
             <div className="p-3" >
-                <div className="flex w-full  text-white font-bold max-w-[1400px] mx-auto justify-between p-5">
+                <div className="flex w-full  text-white font-medium max-w-[1400px] mx-auto justify-between p-5">
                     <div>
                         <h4 className="">© 2024 Kickit Web Design</h4>
                     </div>
