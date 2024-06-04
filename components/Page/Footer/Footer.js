fc736f91b5640645c663f5e9ed8bfb2394c5bf2b
sahilsian <sahilsian101@gmail.com>
2024-06-04 20:30:01 -0300

    fix: footer misplacement

--- components/Page/Footer/Footer.js
+++ components/Page/Footer/Footer.js
@@ -9,7 +9,7 @@ import { CallToActionButton } from "../../Custom/CallToActionButton"
 
 export const Footer = ({ items, callToActionLabel, callToActionDestination }) => {
     return (
-        <div style={{backgroundColor: siteConfig.colors.solids.dark}} className="mt-7">
+        <div style={{ backgroundColor: siteConfig.colors.solids.dark }} className="mt-7">
 
             <div className="flex w-full text-white flex-wrap max-w-[1400px] gap-12 py-[50px] mx-auto p-5">
                 <div className="flex-1 w-full">
@@ -32,22 +32,29 @@ export const Footer = ({ items, callToActionLabel, callToActionDestination }) =>
                             <FontAwesomeIcon icon={faPhone} width={18}></FontAwesomeIcon>
                             <h4 className="text-[18px] font-medium">{siteConfig.texts.footer.phone}</h4>
                         </div>
-
+                        {siteConfig.texts.footer.location_dest && 
                         <Link href={siteConfig.texts.footer.location_dest} target="_blank" className="flex items-center gap-5 mb-7">
                             <FontAwesomeIcon icon={faMapPin} width={18}></FontAwesomeIcon>
                             <h4 className="text-[18px] font-medium">{siteConfig.texts.footer.location}</h4>
                         </Link>
+                        }
 
                         <div className="flex items-center gap-5 mb-3">
+                            {siteConfig.texts.footer.instagram &&
                             <Link className="hover:opacity-75 transition-all" href={siteConfig.texts.footer.instagram}>
-                            <FontAwesomeIcon icon={faInstagram} width={18}></FontAwesomeIcon>
+                                <FontAwesomeIcon icon={faInstagram} width={18}></FontAwesomeIcon>
                             </Link>
-                            <Link  className="hover:opacity-75 transition-all" href={siteConfig.texts.footer.facebook}>
-                            <FontAwesomeIcon icon={faFacebook} width={18}></FontAwesomeIcon>
+                            }
+                            {siteConfig.texts.footer.facebook &&
+                            <Link className="hover:opacity-75 transition-all" href={siteConfig.texts.footer.facebook}>
+                                <FontAwesomeIcon icon={faFacebook} width={18}></FontAwesomeIcon>
                             </Link>
-                            <Link  className="hover:opacity-75 transition-all" href={siteConfig.texts.footer.tiktok}>
-                            <FontAwesomeIcon icon={faTiktok} width={18}></FontAwesomeIcon>
+                            }
+                            {siteConfig.texts.footer.tiktok && 
+                            <Link className="hover:opacity-75 transition-all" href={siteConfig.texts.footer.tiktok}>
+                                <FontAwesomeIcon icon={faTiktok} width={18}></FontAwesomeIcon>
                             </Link>
+                            }
                         </div>
                         <div>
                             <h5 className="py-5 font-medium text-[20px]">{siteConfig.texts.footer.cta}</h5>
@@ -61,20 +68,20 @@ export const Footer = ({ items, callToActionLabel, callToActionDestination }) =>
                         <h2 className="text-[1.7rem] lg:text-[2rem] mb-3 font-medium">Pages</h2>
                     </div>
                     <div className="flex flex-col flex-wrap max-h-[280px]">
-                    {items.map((item)=> {
-                        return (
-                            <div>
-                            <Link className="block text-[14px] pb-2 hover:opacity-80 transition-all" href={item.destination}>{item.label}</Link>
-                            {item.subMenuItem.map((subitem) => {
-                                return (
-                                    <div>
-                                    <Link className="block text-[14px] pb-2 hover:opacity-80 transition-all" href={subitem.destination}>{subitem.label}</Link> 
-                                    </div>
-                                )
-                            })}
-                            </div>
-                        )
-                    })}
+                        {items.map((item) => {
+                            return (
+                                <div>
+                                    <Link className="block text-[14px] pb-2 hover:opacity-80 transition-all" href={item.destination}>{item.label}</Link>
+                                    {item.subMenuItem.map((subitem) => {
+                                        return (
+                                            <div>
+                                                <Link className="block text-[14px] pb-2 hover:opacity-80 transition-all" href={subitem.destination}>{subitem.label}</Link>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            )
+                        })}
                     </div>
 
                 </div>
@@ -82,7 +89,7 @@ export const Footer = ({ items, callToActionLabel, callToActionDestination }) =>
             <div className="p-3" >
                 <div className="flex w-full  text-white font-medium max-w-[1400px] mx-auto justify-between p-5">
                     <div>
-                        <h4 className="">© 2024 Bloom Bug Screens</h4>
+                        <h4 className="">© {siteConfig.texts.footer.copyright}</h4>
                     </div>
                     <div className="flex gap-5">
                         <Link className="transition-all opacity-70 hover:opacity-100" href={"/company/privacy-policy"}>Privacy Policy</Link>
